8def52790f7c3aac68b17f719f48e80d6a42f4c3
Joker <3061301437@qq.com>
2022-05-18 20:16:29 +0800

    first commit

---
+++ src/main/java/com/joker/GitTest.java
@@ -0,0 +1,12 @@
+package com.joker;
+
+/**
+ * @author joker
+ * @create 2022-05-18
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+    }
+}
